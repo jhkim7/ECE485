d3a5ed11cb98044837f925b394d3e6a7079f0ecd
hrcerqueira@gmail.com <jhkim7@ncsu.edu>
2014-11-20 08:44:57 -0500

    Beta completed

--- dist/index.js
+++ dist/index.js
@@ -10,19 +10,55 @@ var app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
     , fs = require('fs')
     , five = require("johnny-five"),
-    board, servo, led, sensor;
-
-var seven, eight;
+    board, low, volup, high, voldn, dimmer, left, right, trup, trdn, src, kill, led;
+var wiper = 128;
+var lock = false;
+var headlightState = 0;
 
 board = new five.Board();
 
 // On board ready
 // Init a led on pin 13, strobe every 1000ms
 board.on("ready", function () {
+    low = new five.Led(2);
+    volup = new five.Led(3);
+    high = new five.Led(4);
+    voldn = new five.Led(5);
+    dimmer = 6;
+    this.pinMode(dimmer, five.Pin.PWM);
+    left = new five.Led(7);
+    right = new five.Led(8);
+    trup = new five.Led(9);
+    trdn = new five.Led(10);
+    src = new five.Led(11);
+    kill = new five.Button(12);
     led = new five.Led(13);
     led.off();
-    seven = new five.Led(7);
-    eight = new five.Led(8);
+
+    kill.on("release", function () { // Kill Radio pins if necessary.
+        lock = true;
+        volup.off();
+        voldn.off();
+        trup.off();
+        trdn.off();
+        src.off();
+        led.off();
+        console.log("Button Pressed.");
+    });
+
+    kill.on("hit", function () {
+        console.log("Button Released.");
+        voldn.on();
+        led.strobe();
+        board.wait(5000, function () {
+            lock = false;
+            voldn.off();
+            led.stop();
+            led.off();
+        });
+        console.log("Routine Finished.");
+    });
+
     console.log("The Board is ready.");
 });
 
@@ -31,15 +67,40 @@ app.listen(8080);
 
 // Handle web server
 function handler(req, res) {
+
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
     fs.readFile(__dirname + '/index.html',
         function (err, data) {
             if (err) {
                 res.writeHead(500);
                 return res.end('Error loading index.html');
             }
-            res.writeHead(200);
-            res.end(data);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
         });
+    } else if (req.url.indexOf('.css') != -1) { //req.url has the pathname, check if it conatins '.css'
+        fs.readFile(__dirname + '/' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url.indexOf('.js') != -1) {
+        fs.readFile(__dirname + "/" + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'application/javascript' });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url.indexOf('.wav') != -1) {
+        fs.readFile(__dirname + "/" + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'audio/wav' });
+            res.write(data);
+            res.end();
+        });
+    }
 }
 
 // On a socket connection
@@ -49,19 +110,108 @@ io.sockets.on('connection', function (socket) {
     // When the socket receives the 'gesture' message
     socket.on('gesture', function (data) {
         if (board.isReady) {
-            if(data.grabStrength){
-                led.on();
-            } else led.off();
-        }
-        if(board.isReady) {
-            if(data.swipeRight) {
-                console.log("it came into a swipe right function of the socket");
-                led.toggle();
-                seven.toggle();
+            if (!lock) {
+                if (data.gestureOff) {
+                    led.off();
+                    volup.off();
+                    voldn.off();
+                    trup.off();
+                    trdn.off();
+                    src.off();
+                    console.log("Gesture Off.");
+                }
+                if (data.swipeRight) { // Process a right swipe
+                    console.log("Gesture: Swipe Right.             Function: Track/Seek Up.");
+                    trup.on();
+                    led.on();
+                }
+                if (data.swipeLeft) { // Proess a left swipe
+                    console.log("Gesture: Swipe Left.              Function: Track/Seek Down.");
+                    trdn.on();
+                    led.on();
+                }
+                if (data.swipeUp) { // Process an up swipe
+                    if (headlightState < 2) {
+                        if (headlightState < 1) {
+                            console.log("Gesture: Swipe Up.                Function: Lowbeams On.");
+                            low.on();
+                            led.on();
+                            board.analogWrite(dimmer, wiper);
+                            headlightState = 1;
+                        } else {
+                            console.log("Gesture: Swipe Up.                Function: Highbeams On.");
+                            low.off();
+                            high.on();
+                            led.on();
+                            headlightState = 2;
+                        }
+                    } else console.log("Gesture: Swipe Up.                Function: None.")
+                }
+                if (data.swipeDown) { // Process a down swipe
+                    if (headlightState > 0) {
+                        if (headlightState > 1) {
+                            console.log("Gesture: Swipe Down.              Function: Highbeams Off.");
+                            high.off();
+                            low.on();
+                            led.on();
+                            headlightState = 1;
+                        } else {
+                            console.log("Gesture: Swipe Down.              Function: Lowbeams Off.");
+                            low.off();
+                            led.on();
+                            board.analogWrite(dimmer, 0);
+                            headlightState = 0;
+                        }
+                    } else console.log("Gesture: Swipe Down.              Function: None.")
+                }
+                if (data.cwCircle) { // Process a cw circle
+                    console.log("Gesture: Clockwise Circle.        Function: Volume Up.");
+                    volup.on();
+                    led.on();
+                }
+                if (data.ccwCircle) { // Process a ccw circle
+                    console.log("Gesture: Counterclockwise Circle. Function: Volume Down.");
+                    voldn.on();
+                    led.on();
+                }
+                if (data.turnLeft) { // Process a left point
+                    console.log("Gesture: Pointing Left.           Function: Left Turn Signal.");
+                    left.on();
+                    left.strobe(700);
+                }
+                if (data.turnRight) { // Process a right point
+                    console.log("Gesture: Pointing Right.          Function: Right Turn Signal.");
+                    right.on();
+                    right.strobe(700);
+                }
+                if (data.turnOff) { // Automatically shut off turn signals
+                    left.stop();
+                    left.off();
+                    right.stop();
+                    right.off();
+                }
+                if (data.screenTap) { // Process a screen tap
+                    console.log("Gesture: Screen Tap.              Function: Radio Source.");
+                    src.on();
+                    led.on();
+                }
+                if (data.keyTap) { //Process a key tap
+                    console.log("Gesture: Key Tap.                 Function: Unused.");
+                }
             }
-            if(data.swipeLeft) {
-                eight.toggle();
-                console.log("it came into a swipe left function of the socket");
+        }
+    });
+
+    socket.on('dimmer', function (data) {
+        if (board.isReady) {
+            if (headlightState > 0) {
+                //console.log(data);
+                var delta = 2 * parseInt(data.dimmerChange);
+                wiper += delta;
+                //console.log(wiper);
+                if (wiper > 255) wiper = 255;
+                else if (wiper < 0) wiper = 0;
+                board.analogWrite(dimmer, wiper);
             }
         }
     });
